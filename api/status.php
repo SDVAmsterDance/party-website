9a62469e1b971088c71bef9480f4a39593437a53
Michael van der Werve <mvdwerve@outlook.com>
2016-05-09 23:52:00 +0200

    The server now correctly talks JSON to us.

--- api/status.php
+++ api/status.php
@@ -8,4 +8,19 @@
  *  @author Michael van der Werve
  */
 
+require_once 'config.php';
 
+// get a database connection
+$dbh = new PDO('mysql:host='.DB_HOST.';dbname='.DB_DATABASE, DB_USER, DB_PASSWORD);  
+
+$regular = 0;
+$vips = 0;
+
+// make the statement
+foreach ($dbh->query("SELECT SUM(guests) + COUNT(id) AS regular, SUM(vips) as vip FROM registrations", PDO::FETCH_ASSOC) as $row) {
+    $regular += $row['regular'];
+    $vips += $row['vip'];
+}
+
+// return the amount of tickets left
+echo json_encode(array("regular" => (70 - $regular), "vip" => min(10 - $vips, 70 - $regular)));
