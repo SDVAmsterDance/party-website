60eff455b7cdf634762753059f6ef82f3ab5c8d3
BrechjeBoeklagen <brechjeboeklagen@gmail.com>
2016-05-09 23:42:23 +0200

    emailll

--- email.php
+++ email.php
@@ -23,21 +23,19 @@
 	<div class="stories clearfix">
 	<div id="story-initial" class="story"> 
 		<div class="story-internal">
-		<b><div class="calligraphy love">D</div>ear <?php echo $registration->buyer(); ?>, </b> thank you for your registration. It will be an honour to receive you 
+		<b><div class="calligraphy love">D</div>ear <?php echo $registration->buyer(); ?>, </b>it will be an honour to receive you 
 		and your guests at the <p class="love">Royal Disney Ball</p>. We will expect you at the <p class="love">Grand Cafe Frankendael</p>, Amsterdam at approximately 20:00. The ball will end 
 		at <p class="love">midnight</p>, so Cinderella can stay untill the very end. 
 		</div>
-	</div>
-	
-	<div class="stories clearfix">
-	<div id="story-initial" class="story"> 
 		<div class="story-internal">
-		<b><div class="calligraphy love">W</div>e hereby confirm your registration.<?php echo $registration->buyer(); ?>, </b> thank you for your registration. It will be an honour to receive you 
-		and your guests at the <p class="love">Royal Disney Ball</p>. We will expect you at the <p class="love">Grand Cafe Frankendael</p>, Amsterdam at approximately 20:00. The ball will end 
-		at <p class="love">midnight</p>, so Cinderella can stay untill the very end. 
+		<div class="calligraphy love">W</div>e hereby confirm your registration. We want to make sure that our messengers understood your registration correctly, so we will sum up the facts.
+		
 		</div>
-	</div>
-	
+		<div class="story-internal">
+		<div class="calligraphy love">W</div>ould you like to store the money you owe us to the bank account SDV Amsterdance <p class="love">NL72ABNA0455257221</p> and mention your name and the subject of the transfer (EindfeeCie)?
+		Thank you very much!
+		</div>
+	</div>	
 	
 </div>
 </div>
