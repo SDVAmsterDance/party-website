5546213b4b9ea9af5b334d0cd593d241e9da4a2d
Michael van der Werve <mvdwerve@outlook.com>
2016-05-20 21:41:50 +0200

    paid now on top

--- api/class.Registrations.php
+++ api/class.Registrations.php
@@ -20,7 +20,7 @@ class Registrations {
         $dbh = new PDO('mysql:host='.DB_HOST.';dbname='.DB_DATABASE, DB_USER, DB_PASSWORD); 
 
         // find all hashes
-        $hashes = $dbh->query("SELECT hash FROM registrations", PDO::FETCH_ASSOC); 
+        $hashes = $dbh->query("SELECT hash FROM registrations ORDER BY paid ASC", PDO::FETCH_ASSOC); 
     
         foreach ($hashes as $row) {
             array_push($this->registrations, Registration::get($row['hash']));
